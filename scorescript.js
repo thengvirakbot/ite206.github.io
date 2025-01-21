e8baf84c11d706481038edbdfdbeb0ae2ecc540c
Theng Virakbot <163020818+thengvirakbot@users.noreply.github.com>
2025-01-21 15:02:36 +0700

    Update scorescript.js

--- scorescript.js
+++ scorescript.js
@@ -1,3 +1,13 @@
+if (!sessionStorage.getItem('userLoggedIn')) {
+    window.location.href = 'index.html';
+}
+
+  document.getElementById('logoutBtn').addEventListener('click', function() {
+    sessionStorage.removeItem('userLoggedIn');
+    
+    window.location.href = 'index.html';
+  });
+
 // localStorage function
 document.addEventListener('DOMContentLoaded', function () {
   loadScoresFromLocalStorage(); 
